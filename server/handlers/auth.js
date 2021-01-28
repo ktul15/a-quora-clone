438c3f647cde55aca39146a96a0c743e59e4a56d
Ketul Makwana <ketulmakwana09@gmail.com>
2021-01-28 18:43:26 +0530

    Add login route

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -7,10 +7,10 @@ module.exports.signUp = async (req, res, next) => {
   const { name, email, password } = req.body
 
   try {
-    // 2. check if user already exists, throw error if it exists
+    // 2. check if user already exists, throw error if yes
     const user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [email])
     if (user.rows.length !== 0) {
-      return res.status(401).json('User already exists. Log in instead!')
+      return res.status(401).json('User already exists. Log in!')
     }
 
     // 3. bcrypt the password
@@ -36,5 +36,32 @@ module.exports.signUp = async (req, res, next) => {
 }
 
 module.exports.login = async (req, res, next) => {
-  res.json('login')
+  try {
+    // 1. get email and password from req.body
+    const { email, password } = req.body
+
+    // 2. check if user already exists, throw error if not
+    const user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [email])
+    if (user.rows.length === 0) {
+      return res.status(401).json(`User doesn't exists. Sign Up!`)
+    }
+
+    const { user_id, user_name, user_email } = user.rows[0]
+
+    // 3. Check if password is valid
+    const isPasswordCorrect = await bcrypt.compare(password, user.rows[0].password)
+    if (!isPasswordCorrect) {
+      return res.status(401).json('Incorrect password or email!')
+    }
+
+    // 4. give them the jwt token if password correct
+    const token = jwtGenerator(user_id, user_name, user_email)
+    res.json({ token })
+  }
+  catch (error) {
+    return next({
+      status: 400,
+      message: error.message
+    })
+  }
 }
